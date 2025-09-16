2dbe242c0976e3b7741b6b6513913a0953006b51
khushdeep5 <babrah.khushdeep268@gmail.com>
2025-09-16 13:56:50 -0600

    root page updated

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,10 +3,9 @@ import Link from "next/link";
 export default function StudentInfo(){
   return(
     <div>
-      <h2>Sudent Info</h2>
-      <p>Khushdeep Kaur</p>
+      <p>Name: Khushdeep Kaur</p>
       <p>
-        <Link href="https://github.com/khushdeep5/cprg306-assignments.git" > My repository </Link>
+        Github: <Link href="https://github.com/khushdeep5/cprg306-assignments.git" > khushdeep5/cprg306-assignments</Link>
       </p>
     </div>
   );
