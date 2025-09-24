8909ba92d983c641721a1c88388a2b9e2f4495d2
khushdeep5 <babrah.khushdeep268@gmail.com>
2025-09-24 15:32:51 -0600

    updated week3 assignment

--- app/page.js
+++ app/page.js
@@ -5,8 +5,11 @@ export default function RootPage(){
     <main>
           <h1>CPRG 306: Web Development 2 - Assignments</h1>
           <p>
-            <Link href="./week-2">Go to week 2</Link></p>
+            <Link href="./week-2">Go to week 2</Link> &rarr; 
+            <Link href="./week-3">Go to week 3</Link></p>
           
         </main>
   )
+
+  
 }
