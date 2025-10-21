7ddd32da8fe07cc4629f34e12ed7b00293cc31de
khushdeep5 <babrah.khushdeep268@gmail.com>
2025-10-20 18:11:56 -0600

    added week-6

--- app/page.js
+++ app/page.js
@@ -8,7 +8,8 @@ export default function RootPage(){
             <Link href="./week-2">Go to week 2</Link> &rarr; 
             <Link href="./week-3">Go to week 3</Link> &rarr; 
             <Link href="./week-4">Go to week 4</Link> &rarr; 
-            <Link href="./week-5">Go to week 5</Link> 
+            <Link href="./week-5">Go to week 5</Link> &rarr; 
+            <Link href="./week-6">Go to week 6</Link> 
             </p>
           
         </main>
---
+++ app/week-6/page.js
@@ -0,0 +1,10 @@
+import ItemList from "./item-list";
+export default function Page() {
+  return(
+    <main>
+    <h1 className="text-white text-5xl text-center">Shopping List</h1>
+    <ItemList/>
+
+    </main>
+  );
+}
